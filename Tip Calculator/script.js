b7dea0992ac753e4e358809ab86e6b5abb9553cf
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-10-21 00:18:37 +0300

    Tip Calculator using arrays and func

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -3,9 +3,24 @@ const calcTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips)
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const total = [];
 
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(total);
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    total.push(tip + bills[i]);
+}
+console.log(bills, tips, total);
+
+const calcAvarage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum / arr.length;
+}
+console.log(calcAvarage(total));
