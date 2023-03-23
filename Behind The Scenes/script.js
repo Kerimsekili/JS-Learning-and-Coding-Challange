8bcb0d67adb57f04ff2d8b5e079c1c2cdd6fbbb2
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-03-23 18:52:10 +0300

    Regular functions vs arrow func

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -32,33 +32,74 @@
 
 // console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAge(2000);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAge(2000);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAgeArrow(2000);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAgeArrow(2000);
+
+// const kerim = {
+//   year: 2000,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// kerim.calcAge();
+
+// const matil = {
+//   year: 2017,
+// };
+
+// matil.calcAge = kerim.calcAge;
+// matil.calcAge();
+
+// const f = kerim.calcAge;
+// f();
+
+var firstName = 'Zeynep';
 
 const kerim = {
+  firstName: 'Kerim',
   year: 2000,
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
+    //Solution 1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    //Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
 };
+kerim.greet();
 kerim.calcAge();
 
-const matil = {
-  year: 2017,
+const addExp = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
+addExp(2, 5);
+addExp(2, 5, 7);
 
-matil.calcAge = kerim.calcAge;
-matil.calcAge();
-
-const f = kerim.calcAge;
-f();
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 7);
