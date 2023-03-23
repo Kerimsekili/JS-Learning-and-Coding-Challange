a866bbf25e61152997912bec84d0d2dfa5fac979
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-03-23 16:13:27 +0300

    This keyword in practice

--- Behind The Scenes/hoistingandtcz.js
+++ Behind The Scenes/hoistingandtcz.js
@@ -1,43 +1,43 @@
-'use strict';
+// 'use strict';
 
-// Variables
-console.log(me);
-// console.log(job);
-// console.log(year);
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-var me = 'kerim';
-let job = 'teacher';
-const year = 2000;
+// var me = 'kerim';
+// let job = 'teacher';
+// const year = 2000;
 
-// Functions
-console.log(addDecl(2, 3));
-//console.log(addExp(2, 3));
-console.log(addArrow);
+// // Functions
+// console.log(addDecl(2, 3));
+// //console.log(addExp(2, 3));
+// console.log(addArrow);
 
-function addDecl(a, b) {
-  return a + b;
-}
+// function addDecl(a, b) {
+//   return a + b;
+// }
 
-const addExp = function (a, b) {
-  return a + b;
-};
+// const addExp = function (a, b) {
+//   return a + b;
+// };
 
-var addArrow = (a, b) => a + b;
+// var addArrow = (a, b) => a + b;
 
-// Example
-console.log(numProduct);
-if (!numProduct) deleteShoppingCart();
+// // Example
+// console.log(numProduct);
+// if (!numProduct) deleteShoppingCart();
 
-var numProduct = 10;
+// var numProduct = 10;
 
-function deleteShoppingCart() {
-  console.log('All products deleted!');
-}
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
 
-var x = 1;
-let y = 2;
-const z = 3;
+// var x = 1;
+// let y = 2;
+// const z = 3;
 
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -1,31 +1,64 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
 
-  function printAge() {
-    let output = `${firstName} You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName} You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 2000) {
-      const firstName = 'furkan';
-      var str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 2000) {
+//       const firstName = 'furkan';
+//       var str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      output = 'new output';
-    }
-    // console.log(str);
-    console.log(output);
-  }
+//       output = 'new output';
+//     }
+//     // console.log(str);
+//     console.log(output);
+//   }
 
-  printAge();
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = 'Kerim';
+// const firstName = 'Kerim';
+// calcAge(2000);
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
 calcAge(2000);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAgeArrow(2000);
+
+const kerim = {
+  year: 2000,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+kerim.calcAge();
+
+const matil = {
+  year: 2017,
+};
+
+matil.calcAge = kerim.calcAge;
+matil.calcAge();
+
+const f = kerim.calcAge;
+f();
