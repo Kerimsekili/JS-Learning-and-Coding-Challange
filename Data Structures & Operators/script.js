7208c8ca45e4d3a9e7765159eef3a3d25b4114dc
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-04-09 15:26:37 +0300

    Learning Sets

--- Data Structures & Operators/script.js
+++ Data Structures & Operators/script.js
@@ -50,6 +50,35 @@ const restaurant = {
   },
 };
 
+const orderSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pizza',
+  'Pasta',
+]);
+
+console.log(orderSet);
+
+console.log(new Set('Kerim'));
+
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza'));
+orderSet.add('Garlic Bread');
+orderSet.delete('Risotto');
+// orderSet.clear();
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+//Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager', 'Chef'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(new Set('').size);
+
 /*
 /////////////////////////////////
 // Looping objects
