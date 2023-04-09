b82a8eda99e2d508473835cb7336221676d4efaf
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-04-09 16:40:16 +0300

    Maps: Iterator

--- Data Structures & Operators/script.js
+++ Data Structures & Operators/script.js
@@ -50,6 +50,43 @@ const restaurant = {
   },
 };
 
+const question = new Map([
+  ['question', 'What is the best programming lang ?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🥳'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quizz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+//const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to Array
+console.log([...question]);
+console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+
+/*
+///////////////////////////
+// Maps:Fundamentals
 const rest = new Map();
 rest.set('name', 'Çavuşun Yeri');
 rest.set(1, 'Firenze,Italy');
@@ -76,6 +113,8 @@ console.log(rest);
 console.log(rest.size);
 
 // console.log(rest.get([1, 2]));
+
+*/
 
 /*
 /////////////////////////
