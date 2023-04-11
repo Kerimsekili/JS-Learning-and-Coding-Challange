d8986a1b7ed806657253d47c430a0235d88246f2
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-04-11 15:45:33 +0300

    Code Challange 3

--- Data Structures & Operators/script.js
+++ Data Structures & Operators/script.js
@@ -50,6 +50,48 @@ const restaurant = {
   },
 };
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+//console.log(gameEvents.values());
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(
+  `An event happened, on avarage, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on avarage, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min} : ${event}`);
+}
+
+/*
+////////////////////////////
+// Maps
 const question = new Map([
   ['question', 'What is the best programming lang ?'],
   [1, 'C'],
@@ -83,6 +125,8 @@ console.log([...question]);
 console.log(question.entries());
 console.log(...question.keys());
 console.log(...question.values());
+
+*/
 
 /*
 ///////////////////////////
