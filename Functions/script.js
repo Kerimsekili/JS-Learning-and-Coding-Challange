3bafd099004b31a46a0ccc73200daca453f2aa43
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-04-15 00:40:50 +0300

    Functions: Default Values

---
+++ Functions/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('GK342');
+createBooking('AH1223', 2, 3433);
+createBooking('AH1223', undefined, 3433);
