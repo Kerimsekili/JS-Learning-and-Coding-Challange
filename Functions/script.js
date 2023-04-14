1954467dfb5f3684e379508abc1ea58fea024340
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-04-15 01:10:01 +0300

    Passing Arguments : Value vs. Reference

--- Functions/script.js
+++ Functions/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/*
+////////////////////
+//Default Parameters
 const bookings = [];
 
 const createBooking = function (flightNum, numPassengers = 1, price = 199) {
@@ -19,3 +22,31 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 createBooking('GK342');
 createBooking('AH1223', 2, 3433);
 createBooking('AH1223', undefined, 3433);
+*/
+
+const flight = 'LH1234';
+const kerim = {
+  name: 'Kerim Sekili',
+  passport: 50489054751,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'NJS123';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 50489054751) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+// checkIn(flight, kerim);
+// console.log(flight);
+// console.log(kerim);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(kerim);
+checkIn(flight, kerim);
