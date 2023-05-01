f0ca2c67daaf50f8fdc8ce2a086d16d53247725b
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-05-01 10:40:25 +0300

    The bind Method

--- Functions/script.js
+++ Functions/script.js
@@ -151,3 +151,49 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 book.call(swiss, ...flightData);
+
+// Bind method
+//book.call(thy, 344, 'Ero Sikos');
+const bookTHY = book.bind(thy);
+const bookLH = book.bind(luftansa);
+const bookLX = book.bind(swiss);
+
+bookTHY(23, 'Kerim Sekili');
+
+const bookTHY23 = book.bind(thy, 23);
+bookTHY23('Kerim Sekili');
+bookTHY23('Za asda');
+
+// With Event Listeners
+luftansa.planes = 300;
+luftansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+luftansa.buyPlane;
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', luftansa.buyPlane.bind(luftansa));
+
+//Partial Application
+
+const addTax = (rate, value) => value + value * rate;
+
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+//addVAT = value => value + value  * 0.23;
+
+console.log(addVAT(100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
