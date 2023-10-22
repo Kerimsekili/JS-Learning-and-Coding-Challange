d1876ae9d804d6c8871e8e5f1f3d521e9214ddf7
Kerim Sekili <kerimsekilix@gmail.com>
2023-10-22 11:18:03 +0300

    IIFE

--- Functions/script.js
+++ Functions/script.js
@@ -202,39 +202,49 @@ The bind Method
 
 /// Coding Challange 1
 
-const poll = {
-  question: 'What is you rfavorite programming Language ?',
-  options: ['0:JavaScript', '1:Python', '2:Rust', '3: C++'],
-  // This generates [0,0,0,0]. More in the next section
+// const poll = {
+//   question: 'What is you rfavorite programming Language ?',
+//   options: ['0:JavaScript', '1:Python', '2:Rust', '3: C++'],
+//   // This generates [0,0,0,0]. More in the next section
 
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write option number')`
-      )
-    );
-    console.log(answer);
-    // Register answer
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number')`
+//       )
+//     );
+//     console.log(answer);
+//     // Register answer
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
 
-    this.displayResults();
-    this.displayResults('string');
-  },
-  displayResults(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      // Poll results are 13,2,4,1
-      console.log(`Poll results are ${this.answers.join(',')}`);
-    }
-  },
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       // Poll results are 13,2,4,1
+//       console.log(`Poll results are ${this.answers.join(',')}`);
+//     }
+//   },
+// };
+
+// // poll.registerNewAnswer();
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+const runOnce = function () {
+  console.log('This will never run again');
 };
+runOnce();
 
-// poll.registerNewAnswer();
-
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+// IIFE
+(function () {
+  console.log('This will never run again');
+})();
